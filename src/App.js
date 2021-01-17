53ca427a4c0f1d70d9475505ebf1aa2cdd1079aa
Justin <justinhwang92@gmail.com>
2021-01-17 14:26:01 -0500

    Basic Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+// what {fav} means?
+// inside of props, I am going to use the "fav" object
+
+function Food({ fav }) {
+  //console.log(props);            //see what properties are in props
+  return <h1>I like {fav}</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hlello world!</h1>
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="gogi" />
+      <Food fav="pho" />
     </div>
   );
 }
