04027106dc01eda431bcf2aea49a79278e0066c5
Justin <justinhwang92@gmail.com>
2021-01-18 15:38:43 -0500

    Rendering the movies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
   state = {
@@ -11,7 +12,25 @@ class App extends React.Component {
   // But we neet to tell JavaScript, that our getMovies function
   // will take little bit of time, we should wait for it
   getMovies = async () => {
-    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    //const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    //console.log(movies); // check what objects are in there
+    //console.log(movies.data.data.movies);
+
+    // store only for movies.data.data.movies
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+
+    // we are going to put these movies inside of the state
+    // first movies is from the state, another one if from axios
+    // this.setState({movies:movies})
+
+    // shortcut
+    this.setState({ movies, isLoading: false });
   };
 
   componentDidMount() {
@@ -19,8 +38,26 @@ class App extends React.Component {
   }
 
   render() {
-    const { isLoading } = this.state;
-    return <div>{this.state.isLoading ? "Loading..." : "We are ready"}</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? "Loading..."
+          : movies.map((movie) => {
+              //console.log(movie);
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              );
+            })}
+      </div>
+    );
   }
 }
 
