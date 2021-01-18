31abe105857c7fd43001b8ab1bee78ea02da87bc
Justin <justinhwang92@gmail.com>
2021-01-18 15:14:55 -0500

    Fetching Movies from API

--- src/App.js
+++ src/App.js
@@ -1,41 +1,26 @@
 import React from "react";
+import axios from "axios";
 
-// class component
-// -don't have a return, but they have a render method.
-// render method is from React.component.
-// React is automatically going to execute the render method
-// of your class component
 class App extends React.Component {
-  // State is an obecjt, and in state, you are going to
-  // put the data of your component.
-  // The data that will change
   state = {
-    count: 0,
+    isLoading: true,
+    movies: [],
   };
 
-  add = () => {
-    //console.log("add");
-    //this.state.count++;  // Do not mutate state directly. Use setState()  react/no-direct-mutation-state
-    // If we don't use setState, the render function is
-    // not going to be called with new state.
-    this.setState((current) => ({ count: current.count + 1 }));
-    //this.setState({ count: this.state.count + 1 });
+  // When the component mount, we are going to call getMovies()
+  // But we neet to tell JavaScript, that our getMovies function
+  // will take little bit of time, we should wait for it
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
   };
-  minus = () => {
-    //console.log("minus");
-    //this.state.count--;
-    this.setState((current) => ({ count: current.count - 1 }));
-    //this.setState({ count: this.state.count - 1 });
-  };
+
+  componentDidMount() {
+    this.getMovies();
+  }
 
   render() {
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{this.state.isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
 
