d6d9e25a8299cb5a2bd9b1d5aba538421f3a3a05
Justin <justinhwang92@gmail.com>
2021-01-17 19:43:01 -0500

    Add id for images and Refactor

--- src/App.js
+++ src/App.js
@@ -1,41 +1,54 @@
 import React from "react";
 
-function Food({ name, picture }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <img src={picture}></img>
-    </div>
-  );
-}
+// All the element in React should be unique.
+// That's why we need an id on each elements.
 
 const foodILike = [
   {
+    id: 1,
     name: "Kimchi",
     image:
       "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
   },
   {
+    id: 2,
     name: "Samgyeopsal",
     image:
       "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
   },
   {
+    id: 3,
     name: "Bibimbap",
     image:
       "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
   },
   {
+    id: 4,
     name: "Doncasu",
     image:
       "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
   },
   {
+    id: 5,
     name: "Kimbap",
     image:
       "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
   },
 ];
+
+function renderFood(dish) {
+  //console.log(dish);
+  return <Food key={dish.id} name={dish.name} picture={dish.image} />;
+}
+
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} alt={name}></img>
+    </div>
+  );
+}
 
 // map is the javaScript function that takes an array
 // and excute a function in each item of the array
@@ -47,9 +60,7 @@ function App() {
       {/* takes each item(dish) then return 
       the name and the imamge
       dish is the object that currently being processed */}
-      {foodILike.map((dish) => (
-        <Food name={dish.name} picture={dish.image} />
-      ))}
+      {foodILike.map(renderFood)}
     </div>
   );
 }
